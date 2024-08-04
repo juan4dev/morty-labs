e32595665a3d7d6a6e7996aadf22fd40ccaf8732
juan4dev <juanindev@gmail.com>
2024-08-04 13:36:33 +0200

    chore: Add character card component and related files

--- src/componets/main-container/main-container.component.ts
+++ src/componets/main-container/main-container.component.ts
@@ -1,18 +1,28 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Character } from '../../models/character.model';
 import { CharactersService } from '../../services';
+import { CharacterCardComponent } from './components';
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, CharacterCardComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainContainerComponent {
   private charactersService: CharactersService = inject(CharactersService);
   characters$: Observable<Character[]> = this.charactersService.getCharacters();
+  characterInfo: Record<string, Character> = {};
+
+  async makeApiCall(url: string) {
+    let character = await firstValueFrom(
+      this.charactersService.getCharacterInformation(url)
+    );
+
+    this.characterInfo[character.id] = character;
+  }
 }
